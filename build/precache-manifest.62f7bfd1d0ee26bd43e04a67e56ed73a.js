8f775d07220802ea8e97803b2648515dbe56711b
lewis mutonyi <104163162+MutonyiLewis@users.noreply.github.com>
2022-10-19 18:23:40 +0300

    "First build"

---
+++ build/precache-manifest.62f7bfd1d0ee26bd43e04a67e56ed73a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a937ede6bcbfeb3b12fc",
+    "url": "/static/css/main.14cf0631.chunk.css"
+  },
+  {
+    "revision": "a937ede6bcbfeb3b12fc",
+    "url": "/static/js/main.a937ede6.chunk.js"
+  },
+  {
+    "revision": "05f317640d51b82fd187",
+    "url": "/static/js/1.05f31764.chunk.js"
+  },
+  {
+    "revision": "1f7631c5e4a3a7969e31",
+    "url": "/static/css/2.396dedee.chunk.css"
+  },
+  {
+    "revision": "1f7631c5e4a3a7969e31",
+    "url": "/static/js/2.1f7631c5.chunk.js"
+  },
+  {
+    "revision": "3c547525d808ca681550",
+    "url": "/static/js/runtime~main.3c547525.js"
+  },
+  {
+    "revision": "a132479e4921f25524e103e4fc050c2b",
+    "url": "/index.html"
+  }
+];
